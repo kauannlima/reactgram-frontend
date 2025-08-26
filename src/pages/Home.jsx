52490f8bcf255e55ae636080b5793c764c0eb31b
Kauan Lima <kauanalmeidalima1405@gmail.com>
2025-08-26 12:02:54 -0300

    Atualizando Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -61,7 +61,7 @@ const Home = () => {
             ))}
           </div>
         ) : (
-          <div className="py-5">
+<div className="flex items-center justify-center h-screen">
             <h2 className="text-center text-gray-300">
               Ainda não há fotos publicadas,{" "}
               <Link
